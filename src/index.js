154cb2c7bb29319886c474e969214b92285ead41
docscodes <docscodes@gmail.com>
2024-10-13 12:23:30 -0400

    fix docker compose

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const fs = require("fs");
 
-// if (!process.env.PORT) {
-//     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
-// }
+if (!process.env.PORT) {
+    throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
+}
 
 const app = express();
 const PORT = process.env.PORT || 3000;
